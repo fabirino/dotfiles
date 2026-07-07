89ea8d46aac9256889a1442e39ef466684b21a86
FabioS <fabalexandre8@gmail.com>
2026-07-07 11:16:55 +0100

    LuaSnip added

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
---vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
---vim.keymap.set('n', '<leader>ps', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ")});
---end)
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope live LSP Document symbols' })
+vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = 'Telescope live marks' })
--- nvim/.config/nvim/lua/fabirino/set.lua
+++ nvim/.config/nvim/lua/fabirino/set.lua
@@ -10,6 +10,8 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
+vim.opt.ignorecase = true
+
 vim.opt.wrap = true
 -- vim.opt.wrapmargin = 15
 vim.opt.textwidth = vim.o.columns - 50
